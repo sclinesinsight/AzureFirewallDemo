1bb60a0bc8a1d7075905b0b9086aa425cee1cca0
Steve Clines <steve.clines@insight.com>
2020-08-06 14:49:40 -0500

    commit

--- Build-Vnets.ps1
+++ Build-Vnets.ps1
@@ -1,5 +1,5 @@
 $resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$templateUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-storage-account-create/azuredeploy.json"
-$parameterUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-storage-account-create/azuredeploy.json"
+$templateUri = "https://github.com/sclinesinsight/AzureFirewallDemo/blob/master/vnets-template.json"
+$parameterUri = "https://github.com/sclinesinsight/AzureFirewallDemo/blob/master/vnets-parameters.json"
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -Location $location -TemplateParameterUri $parameterUri
