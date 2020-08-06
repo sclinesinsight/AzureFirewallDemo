ffbd956c4d9712841e8e9e13545a99608b056aa0
Steve Clines <steve.clines@insight.com>
2020-08-06 17:32:43 -0500

    commit

--- Build-Vnets.ps1
+++ Build-Vnets.ps1
@@ -3,8 +3,9 @@ Write-host "RG is " $resourceGroupName
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 Write-host "Location is " $location
 # Build Resource Group
-New-azresourcegroup -name $resourcegroupname -location $location
+# New-azresourcegroup -name $resourcegroupname -location $location
 # Issue ARM template for Vnet deployment
 $templateUri = "https://raw.githubusercontent.com/sclinesinsight/AzureFirewallDemo/master/vnets-template.json"
 $parameterUri = "https://raw.githubusercontent.com/sclinesinsight/AzureFirewallDemo/master/vnets-parameters.json"
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterUri $parameterUri
+
